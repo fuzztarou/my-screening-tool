fef144563fbb92baf94b8d375e1d3bf3c19bdd4e
fuzztarou <fuzztarou@gmail.com>
2025-10-04 11:09:33 +0900

    feat: 株価データの必須カラムを定義し、定数モジュールに追加

--- app/constants/quotes.py
+++ app/constants/quotes.py
@@ -4,6 +4,24 @@
 J-Quants API から取得する株価データの処理に使用する定数を定義します。
 """
 
+# 株価データの必須カラム
+QUOTES_COLUMNS_TO_EXTRACT = [
+    "Date",
+    "Code",
+    "Open",
+    "High",
+    "Low",
+    "Close",
+    "Volume",
+    "TurnoverValue",
+    "AdjustmentFactor",
+    "AdjustmentOpen",
+    "AdjustmentHigh",
+    "AdjustmentLow",
+    "AdjustmentClose",
+    "AdjustmentVolume",
+]
+
 # 株価データの数値変換対象カラム
 # https://jpx.gitbook.io/j-quants-ja/api-reference/daily_quotes
 QUOTES_COLUMNS_TO_NUMERIC = [
